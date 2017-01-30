911099a5b5b6bfbf2836596239fe5bfbba146bc7
Kelly McNevin <kellymcnevin@Bruces-MacBook-Pro.local>
2017-01-30 12:58:58 -0500

    add entry screen view controller

---
+++ ProjectFour-KellyMcNevin/EntryScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntryScreenViewController.swift
+//  ProjectFour-KellyMcNevin
+//
+//  Created by Kelly McNevin on 1/30/17.
+//  Copyright © 2017 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class EntryScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
